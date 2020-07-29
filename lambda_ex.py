ea0274581d393cf8a0c8790dd8d884580c09447d
rudhrasingh <38124937+rudhrasingh@users.noreply.github.com>
2020-07-30 03:18:03 +0530

    Update lambda_ex.py

--- lambda_ex.py
+++ lambda_ex.py
@@ -1,6 +1,6 @@
 """ lambda is a anonymous function """
 names=['rudhra','surya','akarsh']
-new_list = map(lambda x:x+' dev',names) # map returns a iterable object
+new_list = map(lambda x:x+' dev',names) # map returns an iterable object
 print(new_list)
 for i in new_list:
     print (i)
